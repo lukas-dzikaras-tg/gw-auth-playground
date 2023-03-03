52b4cbc682d528e804a45d406157add61f81ba9c
Lukas Dzikaras <lukas.dzikaras@transfergo.com>
2023-03-03 12:27:22 +0200

    To the basics

--- src/IntrospectionException.ts
+++
@@ -1,3 +0,0 @@
-export default class IntrospectionException extends Error {
-
-}
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-import {APIGatewayRequestAuthorizerEvent} from "aws-lambda"
-import {handleRequest} from "./handler"
-import Configuration from "./configuration"
-import dotenv from "dotenv"
 
-dotenv.config()
 
-exports.handler = async (event: APIGatewayRequestAuthorizerEvent) => {
-    return await handleRequest(event, getConfiguration())
+exports.handler = async () => {
+    return "Works";
 };
 
-const getConfiguration = () => new Configuration(
-    process.env.TRUSTED_WEB_ORIGINS || "",
-    process.env.COOKIE_NAME_PREFIX || "",
-    process.env.ENCRYPTION_KEY || "",
-    process.env.USE_PHANTOM_TOKEN === "true",
-    process.env.INTROSPECTION_URL || "",
-    process.env.CLIENT_ID || "",
-    process.env.CLIENT_SECRET || "",
-    process.env.ALLOW_TOKEN === "false"
-)
+
